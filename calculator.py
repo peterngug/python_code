f977e3f21b3003566ebae9a53fb7fd4a1f8501ef
peterngug <148774381+peterngug@users.noreply.github.com>
2025-03-11 15:28:41 +0300

    My calculator assignment

---
+++ calculator.py
@@ -0,0 +1,15 @@
+number_1 = float(input("Enter your first number:"))
+operation = input("Enter your operation:")
+number_2 = float (input("Enter your second number:"))
+
+if operation == "+":
+    print(number_1 + number_2)
+elif operation == "-":
+    print(number_1 - number_2)
+elif operation == "*":
+        print(number_1 * number_2)
+elif operation == "/":
+        print(number_1 / number_2)
+else:
+        print("Invalid operation")
+                    # The code above is a calculator that takes two numbers and an operation as input and returns the result of the operation.                          
